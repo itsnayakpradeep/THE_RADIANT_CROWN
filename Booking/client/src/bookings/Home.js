ae254d914715e3db1b86e2d44d45e3b491a1daa8
rokocodegen1 <pradeepnayak274@gmail.com>
2023-08-19 17:05:32 +0530

    Register Form

--- Booking/client/src/bookings/Home.js
+++ Booking/client/src/bookings/Home.js
@@ -1,10 +1,10 @@
 import React from 'react';
 import {useSelector} from 'react-redux';
 const Home = () => {
-    const {user} =  useSelector((state) => ({...state}));
+    // const {user} =  useSelector((state) => ({...state}));
     return (
        <div className='container-fluid h1 p-5 text-center'>
-            HomePage{JSON.stringify(user)}    
+            HomePage   
         </div>
     )
 };
---
+++ Booking/client/src/components/RegisterForm.js
@@ -0,0 +1,52 @@
+import React from 'react'
+
+const RegisterForm = ({ 
+        handleSubmit, 
+        name, 
+        setName, 
+        email, 
+        setEmail, 
+        password, 
+        setPassword
+    }) => {
+  return (
+      <form className="mt-3" onSubmit={handleSubmit}>
+        <div className='form-group'>
+          <label className='form-label mb-3'>Your Name</label>
+          <input 
+            type="text" 
+            className="form-control" 
+            placeholder='Enter name' 
+            value={name} 
+            onChange={(e) =>{setName(e.target.value)}}
+          />
+        </div>
+
+        <div className='form-group mb-3'>
+          <label className='form-label'>Your Email</label>
+          <input
+            type="text" 
+            className="form-control" 
+            placeholder='Enter email' 
+            value={email} 
+            onChange={(e) =>{setEmail(e.target.value)}}
+          />
+        </div>
+
+        <div className='form-group mb-3'>
+          <label className='form-label'>Your Password</label>
+          <input 
+            type="password" 
+            className="form-control" 
+            placeholder='Enter name' 
+            value={password} 
+            onChange={(e) =>{setPassword(e.target.value)}}
+          />
+        </div>
+
+        <button className='btn btn-primary' type="submit">Submit</button> 
+     </form>
+  );
+}
+
+export default RegisterForm;
